f2c7bf0cbb7c25478918ecc4f10a6bc20dd9720c
Adam Gobran <adamgobes@gmail.com>
2016-08-30 18:41:24 -0400

    Create placeTypes.js helper so TypeInput component can generate UI

---
+++ utils/placeTypes.js
@@ -0,0 +1,9 @@
+const placeTypes = {
+    restaurant: 'local_dining',
+    cafe: 'local_cafe',
+    subway_station: 'directions_railway',
+    convenience_store: "local_convenience_store",
+    bar: "local_bar"
+}
+
+export default placeTypes;
