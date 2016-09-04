c290fe080f2c1df00976ee9e0c7e8b47182bcd54
Adam Gobran <adamgobes@gmail.com>
2016-09-04 12:46:19 -0400

    Setup mocha and supertest to test express API

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const express = require('express');
+var express = require('express');
 const path = require('path');
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "public")));
+
+app.get("/places", (req, res) => {
+    res.json({
+        "works": true
+    })
+});
 
 app.get("/", (req, res) => {
     res.sendFile("index.html");
@@ -12,3 +18,5 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("App running on port 3000");
 });
+
+module.exports = app
---
+++ test/placesRequest.js
@@ -0,0 +1,18 @@
+var app = require('../app');
+var supertest = require('supertest');
+
+describe("returns a json response with place search information", (done) => {
+    let request;
+    beforeEach(() => {
+        request = supertest(app)
+            .get("/places")
+            .set('Accept', 'application/json')
+    });
+
+
+    it("returns a json response", (done) => {
+        request
+            .expect('Content-type', /json/)
+            .expect(200, done);
+    });
+});
